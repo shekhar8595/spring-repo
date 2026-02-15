8652b236daafea90973b2b7ae2e98fac944599f1
shekhar.sharma <shekhar.sharma@inadev.com>
2026-02-16 00:46:46 +0530

    spring file updated

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "Spring Boot CI/CD with GKE is Running version2!";
+        return "Spring Boot CI/CD with GKE is Running..working now, thanks!";
     }
 
     @GetMapping("/health")
